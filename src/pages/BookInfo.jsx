778a0ca46c5e29990bbab21828d9a35bc413d0df
Kat <kat.m.deans@gmail.com>
2025-01-12 13:08:21 -0500

    html structuring book Info

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -21,66 +21,100 @@ const BookInfo = ({ books, addToCart, cart }) => {
 
   return (
     <div id="books__body">
-        <main id="books__main">
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        <Link to="/books" className="book__link">
-                            <FontAwesomeIcon icon="arrow-left"></FontAwesomeIcon>
-                        </Link>
-                        <Link to="/books" className="book__link"><h2 className="book__selected--title--top">Books</h2>
-                        </Link>
-                    </div>
-                    <div className="book__selected">
-                        <figure className="book__selected--figure">
-                            <img src={book.url} alt="" className="book__selected--img" />
-                        </figure>
-                        <div className="book__selected--description">
-                            <h2 className="book__selected--title">{book.title}</h2>
-                            <Rating rating={book.rating}/>
-                            <div className="book__selected--price">
-                                <Price originalPrice={book.originalPrice} salePrice={book.salePrice}/>
-                            </div>
-                            <div className="book__summary">
-                                <h3 className="book__summary--title">
-                                    {book.summary}
-                                </h3>
-                                <p className="book__summary--para">
-                                    Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit.
-                                </p>
-                                <p className="book__summary--para">
-                                    Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum dolor sit.
-                                </p>
-                            </div>
-                            {
-                                bookExistsOnCart() ? (
-                                <Link to={`/books/${book.id}`} className="book__link">
-                                <button className="btn">Checkout</button>
-                                </Link>
-                            ) : (
-                            <botton className="btn" onClick={() => addBookToCart(book)}>Add to cart</botton>)
-                            }
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="cart__title">Cart</h2>
+            </div>
+            <div className="cart">
+              <div className="cart__header">
+                <span className="cart__book">Book</span>
+                <span className="cart__quantity">Quantity</span>
+                <span className="cart__total">Price</span>
+              </div>
+              <div className="cart__body">
+                {cart.map((item) => {
+                  const itemPrice = item.salePrice || item.originalPrice;
+                  return (
+                    <div className="cart__item" key={item.id}>
+                      <div className="cart__book">
+                        <img
+                          className="cart__book--img"
+                          src={item.url}
+                          alt=""
+                        />
+                        <div className="cart__book--info">
+                          <span className="cart__book--title">
+                            {item.title}
+                          </span>
+                          <span className="cart__book--price">
+                            ${itemPrice.toFixed(2)}
+                          </span>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(item)}
+                          >
+                            Remove
+                          </button>
                         </div>
+                      </div>
+                      <div className="cart__quantity">
+                        <input
+                          type="number"
+                          min={0}
+                          max={99}
+                          className="cart__input"
+                          value={item.quantity}
+                          onChange={(event) =>
+                            changeQuantity(item, event.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="cart__total">
+                        ${(itemPrice * item.quantity).toFixed(2)}
+                      </div>
                     </div>
-                </div>
+                  );
+                })}
+                {/* {(!cart || !cart.length) && <img src={EmptyCart}/>} */}
+                {(!cart || !cart.length) && (
+                  <div className="cart__empty">
+                    <img className="cart__empty--img" src={EmptyCart} alt="" />
+                    <h2>You don't have any books in your cart!</h2>
+                    <Link to="/books">
+                      <button className="btn">Browse books</button>
+                    </Link>
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        <h2 className="book__selected--title--top">Recommended books</h2>
-                    </div>
-                    <div className="books">
-                    {
-                        books
-                        .filter(book => book.rating === 5 && +book.id !== +id)
-                        .slice(0, 4)
-                        .map(book => <Book book={book} key={book.id}/>)
-                    }
-                    </div>                   
+            {cart.length > 0 && (
+              <div className="total">
+                <div className="total__item total__sub-total">
+                  <span>Subtotal</span>
+                  <span>${(total() * 0.9).toFixed(2)}</span>
                 </div>
-            </div>
-        </main>
-
+                <div className="total__item total__tax">
+                  <span>Tax</span>
+                  <span>${(total() * 0.1).toFixed(2)}</span>
+                </div>
+                <div className="total__item total__price">
+                  <span>Total</span>
+                  <span>${total().toFixed(2)}</span>
+                </div>
+                <button
+                  className="btn btn__checkout"
+                  no-cursor
+                  onClick={() => alert(`Haven't got a chance to doing this :)`)}
+                >
+                  Proceed to checkout
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </main>
     </div>
   )
 }

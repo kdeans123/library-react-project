90e501d7d3fbe79df98e10bc420a63953a9789e1
Kat <kat.m.deans@gmail.com>
2025-01-12 12:24:46 -0500

    Loading skeleton

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,30 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import Rating from "./Rating";
 import Price from "./Price";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Book = ({ book }) => {
-  function imageLoaded() {
+  const [img, setImg] = useState();
 
-  }
+  useEffect(() => {
+    const image = new Image()
+    image.src = book.url;
+    image.onload = () => {
+      setImg(image);
+    }
+  })
+
   return (
     <div className="book">
-      <div className="book__img--skeleton"></div>
-      <div className="skeleton book__rating--skeleton"></div>
-
-      {/* <Link to={`/books/${book.id}`} className="">
-        <figure className="book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" onLoad={imageLoaded} />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating}/>   
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/> */}
+      {
+          img ? (
+          <>
+          <Link to={`/books/${book.id}`} className="">
+          <figure className="book__img--wrapper">
+            <img 
+              src={img.src} 
+              alt="" 
+              className="book__img" 
+              />
+          </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating}/>   
+          <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/> 
+          </>
+          ) : (
+            <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+            </>
+          )
+      }
     </div>
   );
 };
@@ -41,3 +62,10 @@ export default Book;
 // we add Link to books/1 routing - it is hard coded for now, 
 //   <Link to={`/books/${book.id}`} className="book__title--link">  we are pushing book.id to the route so it automatically detects the id of the book 
 // to build a skeleton of images loading, we use: onLoad={imageLoaded} 
+// with the skeleton - we need to show it when the books are loading, and show the books when finished loading, we gonna use turnary operator 
+// in the turnary operator we gonna check if there is an image, and if there is 
+// image was not defined so we need to create a hook 
+//  this is creating a DOM image element: useEffect(() => {    const image = new Image()
+// as soon as this book component mounts it is going to call useEffect, it is going to create a DOM image element with java script, and it is going to attach onload function to it: when it loads set this image 
+// now we set image.src element, and src is coming form book, so we are programatially create the image now, we not doing igt in html anymore, we are doing it in java script 
+// this is powerful as now we can use image onload. and then we set th eimage to the image. 
